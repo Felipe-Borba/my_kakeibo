ad18565e12918547399f28fa41a15b98d8a6750a
Felipe Borba <felipe.borba0109@gmail.com>
2024-10-14 01:00:59 -0300

    add another repo implementation

---
+++ lib/data/datasource/user_memory_repository.dart
@@ -0,0 +1,16 @@
+import 'package:my_kakeibo/domain/entity/user.dart';
+import 'package:my_kakeibo/domain/repository/user_repository.dart';
+
+class UserMemoryRepository implements UserRepository {
+  User? _user;
+
+  @override
+  Future<User?> getUser() async {
+    return _user;
+  }
+
+  @override
+  void save(User user) async {
+    _user = user;
+  }
+}
