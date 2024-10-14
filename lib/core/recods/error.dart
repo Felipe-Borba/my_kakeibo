554fbb67ce8881a22668ac1b031b69a9f4029be6
Felipe Borba <felipe.borba0109@gmail.com>
2024-10-13 22:53:19 -0300

    notes

--- lib/core/recods/error.dart
+++ lib/core/recods/error.dart
@@ -10,6 +10,16 @@ final class Empty implements Error {}
 
 /**
  * Exemplo de uso pode ser visto nos teste
+ * Pesquisar sobre o problema do goto quando usa muitos try catch
+ * Parece ter um problema sobre colocar muitos try catch no mesmo fluxo que isso vai minando o código ai cria uns efeitos colaterais difíceis de prever e debugar
+ * 
+ * it's better to only catch exception that I will handle
+ * try {
+ *   //Some code
+ * } on Exception catch(e) {
+ *   //Error handling
+ * }
+ * 
  */
 
 
