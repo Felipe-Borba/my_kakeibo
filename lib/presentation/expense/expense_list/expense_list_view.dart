0f678343d1c542311f959c6eaf827fc8f54d968e
Felipe Borba <felipe.borba0109@gmail.com>
2024-10-29 21:06:03 -0300

    refactor: show_delete_dialog

--- lib/presentation/expense/expense_list/expense_list_view.dart
+++ lib/presentation/expense/expense_list/expense_list_view.dart
@@ -5,6 +5,7 @@ import 'package:my_kakeibo/core/components/app_bar_custom.dart';
 import 'package:my_kakeibo/core/components/drawer_custom.dart';
 import 'package:my_kakeibo/domain/entity/transaction/expense_category.dart';
 import 'package:my_kakeibo/presentation/expense/expense_list/expense_list_controller.dart';
+import 'package:my_kakeibo/core/components/show_delete_dialog.dart';
 
 class ExpenseListView extends StatelessWidget {
   const ExpenseListView({super.key});
@@ -65,31 +66,7 @@ class ExpenseListView extends StatelessWidget {
                               await controller.onDelete(expense);
                             },
                             confirmDismiss: (direction) async {
-                              return await showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title: const Text("Confirm Delete"),
-                                    content: const Text(
-                                      "Are you sure you want to delete this item?",
-                                    ),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop(false);
-                                        },
-                                        child: const Text("Cancel"),
-                                      ),
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop(true);
-                                        },
-                                        child: const Text("Delete"),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
+                              return await showDeleteDialog(context);
                             },
                             child: Padding(
                               padding: const EdgeInsets.symmetric(vertical: 4),
--- lib/presentation/income/income_list/income_list_view.dart
+++ lib/presentation/income/income_list/income_list_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intl/intl.dart';
 import 'package:my_kakeibo/core/components/app_bar_custom.dart';
 import 'package:my_kakeibo/core/components/drawer_custom.dart';
+import 'package:my_kakeibo/core/components/show_delete_dialog.dart';
 import 'package:my_kakeibo/presentation/income/income_list/income_list_controller.dart';
 
 class IncomeListView extends StatelessWidget {
@@ -64,31 +65,7 @@ class IncomeListView extends StatelessWidget {
                               await controller.onDelete(expense);
                             },
                             confirmDismiss: (direction) async {
-                              return await showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title: const Text("Confirm Delete"),
-                                    content: const Text(
-                                      "Are you sure you want to delete this item?",
-                                    ),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop(false);
-                                        },
-                                        child: const Text("Cancel"),
-                                      ),
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop(true);
-                                        },
-                                        child: const Text("Delete"),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
+                              return await showDeleteDialog(context);
                             },
                             child: Padding(
                               padding: const EdgeInsets.symmetric(vertical: 4),
