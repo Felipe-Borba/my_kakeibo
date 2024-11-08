512041db64ba30f71781a515db199fa4b66dfc76
Felipe Borba <felipe.borba0109@gmail.com>
2024-11-07 21:17:08 -0300

    refactor: welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ import 'package:my_kakeibo/domain/use_case/income_use_case.dart';
 import 'package:my_kakeibo/domain/use_case/user_use_case.dart';
 import 'package:my_kakeibo/firebase_options.dart';
 import 'package:my_kakeibo/presentation/expense/expense_form/expense_form_view.dart';
-import 'package:my_kakeibo/presentation/expense/expense_list/expense_list_controller.dart';
 import 'package:my_kakeibo/presentation/expense/expense_list/expense_list_view.dart';
 import 'package:my_kakeibo/presentation/income/income_form/income_form_controller.dart';
 import 'package:my_kakeibo/presentation/income/income_form/income_form_view.dart';
@@ -22,10 +21,8 @@ import 'package:my_kakeibo/presentation/income/income_list/income_list_controlle
 import 'package:my_kakeibo/presentation/income/income_list/income_list_view.dart';
 import 'package:my_kakeibo/presentation/settings/settings_view.dart';
 import 'package:my_kakeibo/presentation/user/create_account/create_account_view.dart';
-import 'package:my_kakeibo/presentation/user/dashboard/dashboard_controller.dart';
 import 'package:my_kakeibo/presentation/user/dashboard/dashboard_view.dart';
 import 'package:my_kakeibo/presentation/user/login/login_view.dart';
-import 'package:my_kakeibo/presentation/welcome/welcome_controller.dart';
 import 'package:my_kakeibo/presentation/welcome/welcome_view.dart';
 
 import 'app.dart';
@@ -56,9 +53,6 @@ class AppModule extends Module {
     i.add(IncomeUseCase.new);
     i.add(UserUseCase.new);
 
-    i.add(WelcomeController.new);
-    i.add(DashboardController.new);
-    i.add(ExpenseListController.new);
     i.add(IncomeFormController.new);
     i.add(IncomeListController.new);
   }
--- lib/presentation/welcome/welcome_view.dart
+++ lib/presentation/welcome/welcome_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:my_kakeibo/presentation/welcome/welcome_controller.dart';
+import 'package:provider/provider.dart';
 
 class WelcomeView extends StatelessWidget {
   const WelcomeView({super.key});
@@ -9,57 +9,63 @@ class WelcomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = Modular.get<WelcomeController>();
+    return ChangeNotifierProvider(
+      create: (context) => WelcomeController(),
+      builder: (context, child) {
+        final controller = Provider.of<WelcomeController>(context);
 
-    return Scaffold(
-      // backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-      body: Padding(
-        padding: const EdgeInsetsDirectional.fromSTEB(16, 24, 16, 24),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.network(
-              'https://images.unsplash.com/photo-1641979319851-a8ed7ed67314?w=1280&h=720',
-              width: double.infinity,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            const Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(16, 8, 16, 0),
-              child: Text('Welcome to our Finance App!'),
-            ),
-            const Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
-              child: Text('Manage your finances with ease.'),
-            ),
-            const Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
-              child: Text('Track your expenses, set budgets, and save money.'),
-            ),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(0),
-                    bottomRight: Radius.circular(0),
-                    topLeft: Radius.circular(0),
-                    topRight: Radius.circular(0),
+        return Scaffold(
+          // backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+          body: Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(16, 24, 16, 24),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://images.unsplash.com/photo-1641979319851-a8ed7ed67314?w=1280&h=720',
+                  width: double.infinity,
+                  height: 240,
+                  fit: BoxFit.cover,
+                ),
+                const Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(16, 8, 16, 0),
+                  child: Text('Welcome to our Finance App!'),
+                ),
+                const Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
+                  child: Text('Manage your finances with ease.'),
+                ),
+                const Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+                  child:
+                      Text('Track your expenses, set budgets, and save money.'),
+                ),
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(0),
+                        bottomRight: Radius.circular(0),
+                        topLeft: Radius.circular(0),
+                        topRight: Radius.circular(0),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+                  child: ElevatedButton(
+                    onPressed: controller.onContinue,
+                    child: const Text("Get Started"),
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
-              child: ElevatedButton(
-                onPressed: controller.onContinue,
-                child: const Text("Get Started"),
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
