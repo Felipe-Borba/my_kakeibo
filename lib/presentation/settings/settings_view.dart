7e5a9952e70cf3a77458fbc84afe5df2c6f8c637
Felipe Borba <felipe.borba0109@gmail.com>
2024-11-10 00:09:57 -0300

    feat: add translation to settings page

--- lib/presentation/settings/settings_view.dart
+++ lib/presentation/settings/settings_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:my_kakeibo/core/components/app_bar_custom.dart';
 import 'package:my_kakeibo/core/components/drawer_custom.dart';
 import 'package:my_kakeibo/presentation/settings/settings_controller.dart';
 import 'package:provider/provider.dart';
@@ -11,10 +13,11 @@ class SettingsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Provider.of<SettingsController>(context);
+    final intl = AppLocalizations.of(context)!;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
+      appBar: AppBarCustom(
+        title: intl.settings,
       ),
       drawer: const DrawerCustom(),
       body: Center(
@@ -24,25 +27,25 @@ class SettingsView extends StatelessWidget {
             DropdownButton<ThemeMode>(
               value: controller.themeMode,
               onChanged: controller.updateThemeMode,
-              items: const [
+              items:  [
                 DropdownMenuItem(
                   value: ThemeMode.system,
-                  child: Text('System Theme'),
+                  child: Text(intl.systemTheme),
                 ),
                 DropdownMenuItem(
                   value: ThemeMode.light,
-                  child: Text('Light Theme'),
+                  child: Text(intl.lightTheme),
                 ),
                 DropdownMenuItem(
                   value: ThemeMode.dark,
-                  child: Text('Dark Theme'),
+                  child: Text(intl.darkTheme),
                 )
               ],
             ),
             ElevatedButton.icon(
               key: const Key("logout"),
               onPressed: controller.logout,
-              label: const Text('Logout'),
+              label:  Text(intl.logout),
               icon: const Icon(Icons.logout),
             ),
           ],
