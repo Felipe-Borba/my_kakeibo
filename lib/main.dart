e7c56ced8eea2e0bbe117a4b00beee5fd443fd79
Felipe Borba <felipe.borba0109@gmail.com>
2025-01-28 09:06:07 -0300

    ajuste na ci

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:my_kakeibo/dependency_manager.dart';
 import 'package:my_kakeibo/firebase_options.dart';
@@ -11,7 +12,9 @@ import 'app.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
-  print('Mensagem recebida com app em segundo plano: ${message.messageId}');
+  if (kDebugMode) {
+    print('Mensagem recebida com app em segundo plano: ${message.messageId}');
+  }
 }
 
 void main() async {
--- lib/main_poc.dart
+++ lib/main_poc.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:my_kakeibo/presentation/core/app_theme.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     final theme = getMaterialTheme(context);
@@ -13,38 +15,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: theme.light(),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
-  final double available = 2000.00;
-  final double goal = 2500.00;
-  final double spent = 600.00;
-
-  final List<Map<String, dynamic>> transactions = [
-    {
-      "date": "Today",
-      "items": [
-        {"title": "Heades", "subtitle": "Subhead", "amount": 30.01},
-        {"title": "Heades", "subtitle": "Subhead", "amount": 30.01},
-      ]
-    },
-    {
-      "date": "Yesterday",
-      "items": [
-        {"title": "Heades", "subtitle": "Subhead", "amount": 30.01},
-        {"title": "Heades", "subtitle": "Subhead", "amount": 30.01},
-      ]
-    }
-  ];
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const double available = 2000.00;
+    const double goal = 2500.00;
+    const double spent = 600.00;
+
+    final List<Map<String, dynamic>> transactions = [
+      {
+        "date": "Today",
+        "items": [
+          {"title": "Head", "subtitle": "Subhead", "amount": 30.01},
+          {"title": "Head", "subtitle": "Subhead", "amount": 30.01},
+        ]
+      },
+      {
+        "date": "Yesterday",
+        "items": [
+          {"title": "Head", "subtitle": "Subhead", "amount": 30.01},
+          {"title": "Head", "subtitle": "Subhead", "amount": 30.01},
+        ]
+      }
+    ];
+
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(
               icon: Icon(Icons.bar_chart), label: "Insights"),
@@ -61,26 +65,27 @@ class HomeScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(height: 16),
-              Text("Bem-vindo, Felipe!",
+              const Text("Bem-vindo, Felipe!",
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               const SizedBox(height: 8),
-              Text("Available",
+              const Text("Available",
                   style: TextStyle(fontSize: 16, color: Colors.grey)),
               Text("R\$ ${available.toStringAsFixed(2)}",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                  style: const TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold)),
               const SizedBox(height: 8),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("meta", style: TextStyle(fontSize: 16)),
+                  const Text("meta", style: TextStyle(fontSize: 16)),
                   Text(goal.toStringAsFixed(2),
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold)),
                 ],
               ),
-              Divider(),
+              const Divider(),
               Text("gasto: ${spent.toStringAsFixed(2)}",
-                  style: TextStyle(fontSize: 14, color: Colors.grey)),
+                  style: const TextStyle(fontSize: 14, color: Colors.grey)),
               const SizedBox(height: 16),
               Expanded(
                 child: ListView(
@@ -92,20 +97,21 @@ class HomeScreen extends StatelessWidget {
                           padding: const EdgeInsets.symmetric(vertical: 8.0),
                           child: Text(
                             group["date"],
-                            style: TextStyle(
+                            style: const TextStyle(
                                 fontSize: 18, fontWeight: FontWeight.bold),
                           ),
                         ),
                         ...group["items"].map<Widget>((item) {
                           return Card(
-                            margin: EdgeInsets.symmetric(vertical: 4),
+                            margin: const EdgeInsets.symmetric(vertical: 4),
                             child: ListTile(
-                              leading: CircleAvatar(child: Text("A")),
+                              leading: const CircleAvatar(child: Text("A")),
                               title: Text(item["title"]),
                               subtitle: Text(item["subtitle"]),
                               trailing: Text(
                                 item["amount"].toStringAsFixed(2),
-                                style: TextStyle(fontWeight: FontWeight.bold),
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
                               ),
                             ),
                           );
